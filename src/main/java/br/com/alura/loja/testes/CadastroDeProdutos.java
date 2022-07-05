a4032ae5c97ad533c7a5eb173bd60fca3cd9b9cc
Sarah Tumenas <sarah.marques@spread.com.br>
2022-07-05 17:29:07 -0300

    Persistindo uma entidade

---
+++ src/main/java/br/com/alura/loja/testes/CadastroDeProdutos.java
@@ -0,0 +1,26 @@
+package br.com.alura.loja.testes;
+
+import br.com.alura.loja.modelo.Produto;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import java.math.BigDecimal;
+
+public class CadastroDeProdutos {
+
+    public static void main(String[] args) {
+        Produto celular = new Produto();
+        celular.setNome("Iphone 13");
+        celular.setDescricao("Iphone 13 Pro Max");
+        celular.setPreco(new BigDecimal("8000.00"));
+
+        EntityManagerFactory factory = Persistence.createEntityManagerFactory("loja");
+        EntityManager em = factory.createEntityManager();
+
+        em.getTransaction().begin();
+        em.persist(celular);
+        em.getTransaction().commit();
+        em.close();
+    }
+}
