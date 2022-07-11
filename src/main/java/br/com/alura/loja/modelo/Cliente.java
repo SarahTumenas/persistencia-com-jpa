b95fd2b5d90fc5af2e4bde0bfd0bf0175fb475ce
Sarah Tumenas <sarah.marques@spread.com.br>
2022-07-11 16:01:16 -0300

    Embeddable

--- src/main/java/br/com/alura/loja/modelo/Cliente.java
+++ src/main/java/br/com/alura/loja/modelo/Cliente.java
@@ -10,16 +10,23 @@ public class Cliente {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    private String nome;
+    @Embedded
+    private DadosPessoais dadosPessoais;
 
-    private String cpf;
 
+    public String getNome() {
+        return this.dadosPessoais.getNome();
+    }
+
+    public String getCpf() {
+        return this.dadosPessoais.getCpf();
+    }
     public Cliente() {
     }
 
     public Cliente(String nome, String cpf) {
-        this.nome = nome;
-        this.cpf = cpf;
+        this.dadosPessoais = new DadosPessoais(nome, cpf);
+
     }
 
     public Long getId() {
@@ -30,19 +37,7 @@ public class Cliente {
         this.id = id;
     }
 
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getCpf() {
-        return cpf;
-    }
-
-    public void setCpf(String cpf) {
-        this.cpf = cpf;
+    public DadosPessoais getDadosPessoais() {
+        return dadosPessoais;
     }
 }
